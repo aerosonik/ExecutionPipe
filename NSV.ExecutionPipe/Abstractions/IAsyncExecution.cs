a045fd4593af2ce04a4153de1787fcde153c8e40
aerosonik <aerosonik@hotmail.com>
2019-07-16 01:59:24 +0300

    middle commit

---
+++ NSV.ExecutionPipe/Abstractions/IAsyncExecution.cs
@@ -0,0 +1,124 @@
+﻿using NSV.ExecutionPipe.Models;
+using NSV.ExecutionPipe.Pipes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSV.ExecutionPipe
+{
+    public interface IAsyncExecutor<M, R>
+    {
+        Task<PipeResult<R>> ExecuteAsync(M model, ILocalCache cache = null);
+    }
+    public interface ISyncExecutor<M, R>
+    {
+        PipeResult<R> ExecuteSync(M model, ILocalCache cache = null);
+    }
+
+    #region SyncSequentialPipe<M, R>
+    public interface ISyncSequentialPipe<M, R> : ISyncExecutor<M, R>
+    {
+        PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed);
+        ISyncSequentialPipe<M, R> UseStopWatch();
+        ISyncSequentialPipe<M, R> UseLocalCacheThreadSafe();
+        ISyncSequentialPipe<M, R> UseLocalCache();
+        ISyncSequentialExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor);
+        ISyncSequentialExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor, bool addif);
+        ISyncSequentialExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor);
+        ISyncSequentialExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor, bool addif);
+        ISyncSequentialPipe<M, R> If(bool condition);
+        ISyncSequentialPipe<M, R> If(Func<M, bool> condition);
+        ISyncSequentialPipe<M, R> EndIf();
+        ISyncSequentialPipe<M, R> Build();
+    }
+    public interface ISyncSequentialExecutorBuilder<M, R>
+    {
+        ISyncSequentialExecutorBuilder<M, R> BreakIfFailed();
+        ISyncSequentialExecutorBuilder<M, R> AllowBreak();
+        ISyncSequentialExecutorBuilder<M, R> UseStopWatch();
+        ISyncSequentialExecutorBuilder<M, R> Label(string label);
+        ISyncSequentialExecutorBuilder<M, R> RetryIfFailed(int count, int timeOutMilliseconds);
+        ISyncSequentialExecutorBuilder<M, R> ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler);
+        ISyncSequentialPipe<M, R> Build();
+    }
+    #endregion
+
+    #region AsyncSequentialPipe<M, R>
+    public interface IAsyncSequentialPipe<M, R> : IAsyncExecutor<M, R>
+    {
+        PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed);
+        IAsyncSequentialPipe<M, R> UseStopWatch();
+        IAsyncSequentialPipe<M, R> UseLocalCacheThreadSafe();
+        IAsyncSequentialPipe<M, R> UseLocalCache();
+        IAsyncSequentialExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor);
+        IAsyncSequentialExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor, bool addif);
+        IAsyncSequentialExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor);
+        IAsyncSequentialExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor, bool addif);
+        IAsyncSequentialPipe<M, R> If(bool condition);
+        IAsyncSequentialPipe<M, R> If(Func<M, bool> condition);
+        IAsyncSequentialPipe<M, R> EndIf();
+        IAsyncSequentialPipe<M, R> Build();
+    }
+    public interface IAsyncSequentialExecutorBuilder<M, R>
+    {
+        IAsyncSequentialExecutorBuilder<M, R> BreakIfFailed();
+        IAsyncSequentialExecutorBuilder<M, R> AllowBreak();
+        IAsyncSequentialExecutorBuilder<M, R> UseStopWatch();
+        IAsyncSequentialExecutorBuilder<M, R> Label(string label);
+        IAsyncSequentialExecutorBuilder<M, R> RetryIfFailed(int count, int timeOutMilliseconds);
+        IAsyncSequentialExecutorBuilder<M, R> ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler);
+        IAsyncSequentialPipe<M, R> Build();
+    }
+
+    #endregion
+
+    #region SyncParallelPipe<M, R>
+    public interface ISyncParallelPipe<M, R> : ISyncExecutor<M,R>
+    {
+        PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed);
+        ISyncParallelPipe<M, R> UseStopWatch();
+        ISyncParallelPipe<M, R> UseLocalCacheThreadSafe();
+        ISyncParallelExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor);
+        ISyncParallelExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor, bool addif);
+        ISyncParallelExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor);
+        ISyncParallelExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor, bool addif);
+        ISyncParallelPipe<M, R> If(bool condition);
+        ISyncParallelPipe<M, R> If(Func<M, bool> condition);
+        ISyncParallelPipe<M, R> EndIf();
+        ISyncParallelPipe<M, R> Build();
+    }
+    public interface ISyncParallelExecutorBuilder<M, R>
+    {
+        ISyncParallelExecutorBuilder<M, R> UseStopWatch();
+        ISyncParallelExecutorBuilder<M, R> Label(string label);
+        ISyncParallelExecutorBuilder<M, R> ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler);
+        ISyncParallelPipe<M, R> Build();
+    }
+    #endregion
+
+    #region AsyncParallelPipe<M, R>
+    public interface IAsyncParallelPipe<M, R> : IAsyncExecutor<M,R>
+    {
+        PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed);
+        IAsyncParallelPipe<M, R> UseStopWatch();
+        IAsyncParallelPipe<M, R> UseLocalCacheThreadSafe();
+        IAsyncParallelExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor);
+        IAsyncParallelExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor, bool addif);
+        IAsyncParallelExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor);
+        IAsyncParallelExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor, bool addif);
+        IAsyncParallelPipe<M, R> If(bool condition);
+        IAsyncParallelPipe<M, R> If(Func<M, bool> condition);
+        IAsyncParallelPipe<M, R> EndIf();
+        IAsyncParallelPipe<M, R> Build();
+    }
+    public interface IAsyncParallelExecutorBuilder<M, R>
+    {
+        IAsyncParallelExecutorBuilder<M, R> UseStopWatch();
+        IAsyncParallelExecutorBuilder<M, R> Label(string label);
+        IAsyncParallelExecutorBuilder<M, R> ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler);
+        IAsyncParallelPipe<M, R> Build();
+    }
+
+    #endregion
+}
---
+++ NSV.ExecutionPipe/Implementations/SyncPipe.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NSV.ExecutionPipe.Models;
+using NSV.ExecutionPipe.Pipes;
+
+namespace NSV.ExecutionPipe
+{
+    public abstract class SyncParallelPipe<M, R> :
+        ISyncParallelPipe<M, R>,
+        ISyncParallelExecutorBuilder<M, R>
+    {
+        public ISyncParallelExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> EndIf()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> ExecuteSync(M model, ILocalCache cache = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> If(bool condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> If(Func<M, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> UseLocalCacheThreadSafe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncParallelPipe<M, R> UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncParallelPipe<M, R> ISyncParallelExecutorBuilder<M, R>.Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncParallelExecutorBuilder<M, R> ISyncParallelExecutorBuilder<M, R>.Label(string label)
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncParallelExecutorBuilder<M, R> ISyncParallelExecutorBuilder<M, R>.ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncParallelExecutorBuilder<M, R> ISyncParallelExecutorBuilder<M, R>.UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public abstract class SyncSequentialPipe<M, R> :
+        ISyncSequentialPipe<M, R>,
+        ISyncSequentialExecutorBuilder<M, R>
+    {
+        public ISyncSequentialExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialExecutorBuilder<M, R> Add(ISyncExecutor<M, R> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialExecutorBuilder<M, R> Add(Lazy<ISyncExecutor<M, R>> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> EndIf()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> ExecuteSync(M model, ILocalCache cache = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> If(bool condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> If(Func<M, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> UseLocalCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> UseLocalCacheThreadSafe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISyncSequentialPipe<M, R> UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.AllowBreak()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.BreakIfFailed()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialPipe<M, R> ISyncSequentialExecutorBuilder<M, R>.Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.Label(string label)
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.RetryIfFailed(int count, int timeOutMilliseconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        ISyncSequentialExecutorBuilder<M, R> ISyncSequentialExecutorBuilder<M, R>.UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class AsyncParallelPipe<M, R> :
+        IAsyncParallelPipe<M, R>,
+        IAsyncParallelExecutorBuilder<M, R>
+    {
+        public IAsyncParallelExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> EndIf()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PipeResult<R>> ExecuteAsync(M model, ILocalCache cache = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> If(bool condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> If(Func<M, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> UseLocalCacheThreadSafe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncParallelPipe<M, R> UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncParallelPipe<M, R> IAsyncParallelExecutorBuilder<M, R>.Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncParallelExecutorBuilder<M, R> IAsyncParallelExecutorBuilder<M, R>.Label(string label)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncParallelExecutorBuilder<M, R> IAsyncParallelExecutorBuilder<M, R>.ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncParallelExecutorBuilder<M, R> IAsyncParallelExecutorBuilder<M, R>.UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class AsyncSequentialPipe<M, R> :
+        IAsyncSequentialPipe<M, R>,
+        IAsyncSequentialExecutorBuilder<M, R>
+    {
+        public IAsyncSequentialExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialExecutorBuilder<M, R> Add(IAsyncExecutor<M, R> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialExecutorBuilder<M, R> Add(Lazy<IAsyncExecutor<M, R>> executor, bool addif)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PipeResult<R> CreateResult(M model, PipeResult<R>[] results, TimeSpan elapsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> EndIf()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PipeResult<R>> ExecuteAsync(M model, ILocalCache cache = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> If(bool condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> If(Func<M, bool> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> UseLocalCache()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> UseLocalCacheThreadSafe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncSequentialPipe<M, R> UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.AllowBreak()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.BreakIfFailed()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialPipe<M, R> IAsyncSequentialExecutorBuilder<M, R>.Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.Label(string label)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.ResultHandler(Func<M, PipeResult<R>, PipeResult<R>> handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.RetryIfFailed(int count, int timeOutMilliseconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        IAsyncSequentialExecutorBuilder<M, R> IAsyncSequentialExecutorBuilder<M, R>.UseStopWatch()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class AbstractPipe<M,R>
+    {
+        protected readonly Queue<ExecutorContainer<M, R>> _executionQueue = new Queue<ExecutorContainer<M, R>>();
+        protected Optional<M> _model = Optional<M>.Default;
+        protected Optional<Stopwatch> _stopWatch = Optional<Stopwatch>.Default;
+        protected Optional<List<PipeResult<R>>> _results = Optional<List<PipeResult<R>>>.Default;
+        protected Optional<IDictionary<object, object>> _localCache = Optional<IDictionary<object, object>>.Default;
+        protected Optional<ILocalCache> _externalCache = Optional<ILocalCache>.Default;
+        protected ExecutorContainer<M, R> _current;
+        private Optional<Stack<(Optional<Func<M, bool>> calculated, Optional<bool> constant)>> _ifConditionStack
+                = Optional<Stack<(Optional<Func<M, bool>> calculated, Optional<bool> constant)>>.Default;
+    }
+}
